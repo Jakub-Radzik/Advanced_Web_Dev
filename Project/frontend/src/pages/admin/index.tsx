6735794c6e254433d12c9cb8992157537504de07
dkrutul <dawtul01@gmail.com>
2023-06-05 12:38:42 +0200

    login test

--- Project/frontend/src/pages/admin/index.tsx
+++ Project/frontend/src/pages/admin/index.tsx
@@ -1,3 +1,43 @@
+import React, { useEffect } from 'react';
+
 export const Admin = () => {
-  return <div>Admin Panel</div>;
+  const handleGoogleLogin = () => {
+    gapi.auth2.getAuthInstance().signIn().then((googleUser: any) => {
+      // Retrieve user information
+      const profile = googleUser.getBasicProfile();
+      const email = profile.getEmail();
+      const name = profile.getName();
+      
+      // You can use the retrieved information for further processing or send it to your server for authentication
+      console.log('Email:', email);
+      console.log('Name:', name);
+    });
+  };
+
+  useEffect(() => {
+    // Load Google API Client script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/platform.js';
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          client_id: 'YOUR_CLIENT_ID',
+        });
+      });
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      // Cleanup
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <button onClick={handleGoogleLogin}>
+      Login with Google
+    </button>
+  );
 };
