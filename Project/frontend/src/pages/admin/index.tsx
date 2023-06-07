a7c37c4c705dcba6e7d205168213159df448e348
dkrutul <dawtul01@gmail.com>
2023-06-07 20:02:17 +0200

    final loginSSO

--- Project/frontend/src/pages/admin/index.tsx
+++ Project/frontend/src/pages/admin/index.tsx
@@ -1,39 +1,17 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export const Admin = () => {
-  const handleGoogleLogin = () => {
-    gapi.auth2.getAuthInstance().signIn().then((googleUser: any) => {
-      // Retrieve user information
-      const profile = googleUser.getBasicProfile();
-      const email = profile.getEmail();
-      const name = profile.getName();
+  const handleGoogleLogin = async () => {
+    try {
+      window.location.replace('http://localhost:8008/api/v1/auth/token');
       
-      // You can use the retrieved information for further processing or send it to your server for authentication
-      console.log('Email:', email);
-      console.log('Name:', name);
-    });
+      
+      
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   };
-
-  useEffect(() => {
-    // Load Google API Client script dynamically
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/platform.js';
-    script.async = true;
-    script.defer = true;
-    script.onload = () => {
-      gapi.load('auth2', () => {
-        gapi.auth2.init({
-          client_id: '181433232492-d3mk5g8rqupbek3g1i99713llp9r60mh.apps.googleusercontent.com',
-        });
-      });
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup
-      document.body.removeChild(script);
-    };
-  }, []);
 
   return (
     <button onClick={handleGoogleLogin}>
