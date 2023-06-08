cf2f94a3af5ac008919c2c268903f885b378f07d
Kamil <herbetko.kamil@gmail.com>
2023-06-08 15:12:19 +0200

    Fixing datetime issues.

--- Project/backend/src/apps/sessions/models.py
+++ Project/backend/src/apps/sessions/models.py
@@ -3,21 +3,22 @@ from tortoise import fields
 from tortoise.models import Model
 from src.apps.movies.models import Movie
 from src.apps.rooms.models import Room
+import pytz
 
 
 class Session(Model):
     id = fields.IntField(pk=True)
-    room_fk = fields.ForeignKeyField('models.Room', related_name='sessions')
-    movie_fk = fields.ForeignKeyField('models.Movie', related_name='sessions')
+    room_fk = fields.ForeignKeyField("models.Room", related_name="sessions")
+    movie_fk = fields.ForeignKeyField("models.Movie", related_name="sessions")
     datetime = fields.DatetimeField()
 
     class Meta:
-        unique_together = ('room_fk', 'datetime')
+        unique_together = ("room_fk", "datetime")
 
 
 class Ticket(Model):
     id = fields.IntField(pk=True)
-    session_fk = fields.ForeignKeyField('models.Session', related_name='tickets')
+    session_fk = fields.ForeignKeyField("models.Session", related_name="tickets")
     seat = fields.IntField()
     row = fields.CharField(max_length=5)
     price = fields.DecimalField(max_digits=5, decimal_places=2)
@@ -27,13 +28,17 @@ class Ticket(Model):
     last_reserved = fields.DatetimeField(null=True)
 
     def is_reserved(self) -> bool:
-        return self.last_reserved is not None and self.last_reserved > datetime.now() - timedelta(minutes=5)
+        return (
+            self.last_reserved is not None
+            and self.last_reserved
+            > pytz.UTC.localize(datetime.now() - timedelta(minutes=5))
+        )
 
     class Meta:
-        unique_together = ('session_fk', 'seat', 'row')
+        unique_together = ("session_fk", "seat", "row")
 
     class PydanticMeta:
-        computed = ('is_reserved',)
+        computed = ("is_reserved",)
 
 
 class TicketReservation(Model):
@@ -44,7 +49,9 @@ class TicketReservation(Model):
     transaction_id = fields.CharField(max_length=255, null=True)
 
     def is_expired(self) -> bool:
-        return self.created_at < datetime.now() - timedelta(minutes=5)
+        return self.created_at < pytz.UTC.localize(
+            datetime.now() - timedelta(minutes=5)
+        )
 
     class PydanticMeta:
-        computed = ('is_expired',)
+        computed = ("is_expired",)
